dd515a2e110d9b658b2c47255c5b56d26784ea72
Harsh Patel <46072683+Harsh-B-Patel@users.noreply.github.com>
2019-02-12 15:37:24 -0500

    Update ShareClass.java

--- flappy rocket/flappy rocket/ShareClass.java
+++ flappy rocket/flappy rocket/ShareClass.java
@@ -4,7 +4,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * A class which acts as sub class of actor
  * has all the actor classes as subclesses
  * 
- * @author (Harsh and dhruv) 
+ * @author Harsh
  * @version 3.1.0
  */
 public class ShareClass extends Actor
